f99a8a95e902f5cfd2715dfebfc2e35371d5bebd
DouglasRodr <wandersondouglasr@gmail.com>
2022-07-31 21:50:04 -0300

    Documentação HTTPS

--- src/main/java/com/hiringcoders/controll/ControllSuggestionsApiApplication.java
+++ src/main/java/com/hiringcoders/controll/ControllSuggestionsApiApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL") })
 @SpringBootApplication
 @EnableScheduling
 public class ControllSuggestionsApiApplication {
